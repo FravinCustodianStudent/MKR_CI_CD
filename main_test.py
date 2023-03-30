dbabf1e8c5e4bc25535e9c0769699c1b5129ccc6
FravinCustodian <3114226@gmail.com>
2023-03-30 11:28:15 +0300

    fix Tests commit

--- main_test.py
+++ main_test.py
@@ -4,21 +4,29 @@ import pytest
 
 from main import ParseFileByName, WriteSearchedArraysInFile
 
+@pytest.fixture
+def create_file():
+    with open('file.txt', 'w') as f:
+        f.write('aboba baobab aboba aboba aboba baobab\n')
+        f.write('baobab baobab\n')
+    yield
+    # cleanup
+    import os
+    os.remove('file.txt')
 
-def test_parseFileByName_non_empty_file():
-    expected_output = [['aboba', 'baobab', 'aboba', 'aboba', 'aboba', 'baobab'], ['baobab', 'baobab']]
+@pytest.mark.parametrize("expected_output",[([['aboba', 'baobab', 'aboba', 'aboba', 'aboba', 'baobab'], ['baobab', 'baobab']])])
+def test_parseFileByName_non_empty_file(expected_output):
     assert ParseFileByName('file.txt') == expected_output
 
 def test_parseFileByName_file_not_found():
     with pytest.raises(FileNotFoundError):
         ParseFileByName('nonexistent_file.txt')
 
-def test_WriteSearchedArraysInFile():
-    output_name = 'file2.txt'
-    input_array = [['aboba', 'baobab', 'aboba', 'aboba', 'aboba', 'baobab'], ['baobab', 'baobab']]
-    searched_word = 'aboba'
-    expected_output = 'aboba baobab aboba aboba aboba baobab \n'
-    WriteSearchedArraysInFile(output_name, searched_word, input_array)
-    with open(output_name) as f:
+@pytest.mark.parametrize("file_name, search_word, input_array, expected_output", [
+    ("file2.txt", "aboba", [['aboba', 'baobab', 'aboba', 'aboba', 'aboba', 'baobab'], ['baobab', 'baobab']], "aboba baobab aboba aboba aboba baobab \n")
+])
+def test_WriteSearchedArraysInFile(file_name, search_word, input_array, expected_output):
+    WriteSearchedArraysInFile(file_name, search_word, input_array)
+    with open(file_name) as f:
         assert f.read() == expected_output
-    os.remove(output_name)
+    os.remove(file_name)
